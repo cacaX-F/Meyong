e05e771a1c0374d84929956277980510e5c0f5b7
cacaX-F <cacanisa246@gmail.com>
2025-05-20 08:38:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.image("20250320_174629.jpg")
-#iniwm
+st.write("apa aja")
