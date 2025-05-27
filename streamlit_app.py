2fd11bc1fc7d08431a91b14ce7cd9c449af6438a
cacaX-F <cacanisa246@gmail.com>
2025-05-27 08:08:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,12 @@ st.write(
 )
 st.image("20250320_174629.jpg")
 st.write("apa aja")
+
+
 st.tiltle("Aplikasi Sederhana")
 st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
 angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
 if (angka % 2) == 0:
     st.write(f"{angka} adalah bilangan genap")
 else:
